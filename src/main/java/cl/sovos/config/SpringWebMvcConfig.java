15a2332984eb8d15d3fb2c2275442629f21e7559
marcosene <marcoaurelio.sene@gmail.com>
2018-05-20 17:31:02 -0400

    Commit remoto

---
+++ src/main/java/cl/sovos/config/SpringWebMvcConfig.java
@@ -0,0 +1,30 @@
+package cl.sovos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by marcosene on 19/05/2018.
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class SpringWebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/resources/static/");
+    }
+
+}
---
+++ src/main/java/cl/sovos/utils/Constants.java
@@ -0,0 +1,16 @@
+package cl.sovos.utils;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Created by marcosene on 19/05/2018.
+ */
+public final class Constants {
+
+    public static final Charset CHARSET_DEFAULT = StandardCharsets.UTF_8;
+    public static final int MAX_SPLIT_FILES = 20;
+
+    private Constants() {
+    }
+}
